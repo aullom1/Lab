eeb394561c50443326a864dac652937b4ff25f4d
Aaron Ullom <Aaron.Ullom@cunamutual.com>
2022-03-14 08:48:20 -0500

    Add sleep

--- PLS-for-onprem-workloads-haproxy/linked/vm_setup.sh
+++ PLS-for-onprem-workloads-haproxy/linked/vm_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sleep 10
 apt-get -y update
+sleep 10 # Not sure why, but the next command fails about half the time.
 apt-get -y install nginx
+sleep 10
 hostname > /var/www/html/index.html
